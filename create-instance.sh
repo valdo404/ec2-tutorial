b9335d5a97d1b409b731c3cf20c3d6a3217a241d
Laurnet VALDES <lvaldes@xebia.fr>
2014-07-02 12:58:43 +0200

    Fix create instance

--- create-instance.sh
+++ create-instance.sh
@@ -1,4 +1,5 @@
 subnet=`cat ec2-subnet`
 ami=ami-76817c1e
 
-ec2-run-instances -s $subnet -k xke-babe $ami -t t2.micro
+instance=`ec2-run-instances -s $subnet -k xke-babe $ami -t t2.micro|grep INSTANCE|cut -f 2`
+echo $instance>ec2-instance-`date +%s`
